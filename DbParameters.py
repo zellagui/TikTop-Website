7e375543b39128f7c45dce31c0b3411ce3581c4f
Zela <83733867+zellagui@users.noreply.github.com>
2022-06-11 00:03:35 -0400

    Update DbParameters.py

--- DbParameters.py
+++ DbParameters.py
@@ -8,7 +8,7 @@ date = date.today()
 #today = date.strftime("%d-%m-%Y")
 today = '10-06-2022'
 collectionName = 'LayerOne' + today
-cluster = MongoClient("mongodb+srv://Tiktop:2022@cluster0.fstq1.mongodb.net/?retryWrites=true&w=majority")
+cluster = MongoClient("ENTER CLUSTER INFO")
 db = cluster["Tiktop"]
 collection = db[collectionName]
 
@@ -16,12 +16,9 @@ results = collection.find({})
 collection.delete_many({})
 
 
-
 #count = 0
 #for x in results:
-  #  file = urllib.request.urlopen(x['downloadAddr'])
- #   #count = file.length + count
+#  file = urllib.request.urlopen(x['downloadAddr'])
+#   #count = file.length + count
 #    count = count +1
 #print(count)
-
-#
